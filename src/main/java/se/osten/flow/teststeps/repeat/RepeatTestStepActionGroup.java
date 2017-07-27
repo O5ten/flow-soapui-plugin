14c4245afd2d960dad5a5a91fa5f72e64e942c9c
mo916607 <Mikael.Ostberg@telenor.se>
2017-07-27 15:20:45 +0200

    Preparation for having a soapui-maven-job that runs a testsuite including the plugin

--- src/main/java/se/osten/flow/teststeps/repeat/RepeatTestStepActionGroup.java
+++ src/main/java/se/osten/flow/teststeps/repeat/RepeatTestStepActionGroup.java
@@ -1,9 +1,7 @@
 package se.osten.flow.teststeps.repeat;
 
 import com.eviware.soapui.impl.wsdl.actions.teststep.WsdlTestStepSoapUIActionGroup;
-import com.eviware.soapui.plugins.ActionConfiguration;
 
-@ActionConfiguration(targetType = RepeatTestStep.class, iconPath = "repeat.jpg")
 public class RepeatTestStepActionGroup extends WsdlTestStepSoapUIActionGroup {
     public RepeatTestStepActionGroup() {
         super("RepeatTestStepActions", "Repeat Test Step Actions");
